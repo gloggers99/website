db21c485fadcfa884b99ac6107ae9d75a6087ece
Lucas Marta <lucas.marta0799@gmail.com>
2026-04-27 13:42:14 -0700

    Update README.md

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,6 @@ import Head from "@/app/sections/head";
 import About from "@/app/sections/about";
 import Portfolio from "@/app/sections/portfolio";
 
-// Tired of William Chastain's BS, so now I'm making
-// an objectively better website than his.
-
 export default function Page() {
     return (
         <main className={"h-screen scroll-smooth overflow-y-scroll snap-y snap-mandatory"}>
