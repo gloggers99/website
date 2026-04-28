4ea63d7fec7404ff12d37ebd5efc0f3d2f641914
Lucas Marta <lucas.marta0799@gmail.com>
2026-04-28 00:23:40 -0700

    Update portfolio WIP text to be smaller.

--- app/sections/portfolio.tsx
+++ app/sections/portfolio.tsx
@@ -30,14 +30,14 @@ export default function Portfolio() {
         <div className={"min-h-screen snap-start flex flex-col"}>
             <TextSpan />
             <div className={"justify-center items-center gap-5 flex flex-col flex-1"}>
-                <h1 className={"text-5xl"}>This section is a work in progress.</h1>
+                <h1 className={"text-3xl"}>This section is a work in progress.</h1>
                 <div className={"flex flex-row items-center gap-5"}>
-                    <p className={"text-2xl"}>For now please refer to my GitHub account:</p>
+                    <p className={"text-xl"}>For now please refer to my GitHub account:</p>
                     <SocialLink icon={<SiGithub size={40} />}
                                 link={"https://github.com/gloggers99"} />
                 </div>
                 <div className={"flex flex-row items-center gap-5"}>
-                    <p className={"text-2xl"}>For inquiries please contact me via email:</p>
+                    <p className={"text-xl"}>For inquiries please contact me via email:</p>
                     <SocialLink icon={<SiGmail size={40}/>}
                                 link={"mailto:lucas.marta0799@gmail.com"} />
                 </div>
