630d2b7b5a310a506a364f39024d0231028c1f6e
Lucas Marta <lucas.marta0799@gmail.com>
2026-04-28 12:02:11 -0700

    Update portfolio section to have Howard Wettan campaign site and sonder.

--- app/sections/portfolio.tsx
+++ app/sections/portfolio.tsx
@@ -1,6 +1,5 @@
-import SocialLink from "@/app/components/socialLink";
-import {SiGithub, SiGmail} from "@icons-pack/react-simple-icons";
 import React from "react";
+import Image from "next/image";
 
 function TextSpan() {
     return (
@@ -25,21 +24,37 @@ function TextSpan() {
     );
 }
 
+function PortfolioItem({name, description, photo}: {
+    name: string,
+    description: string,
+    photo: React.ReactNode
+}) {
+    return (
+        <div className={"flex-1 border-2 rounded flex flex-col items-center"}>
+            <h1 className={"font-playfair text-2xl py-2 text-center"}>{name}</h1>
+            <p className={"font-opensans text-center"}>{description}</p>
+            <div className={"w-full h-full fex-1 relative flex justify-center items-center"}>{photo}</div>
+        </div>
+    );
+}
+
 export default function Portfolio() {
     return (
         <div className={"min-h-screen snap-start flex flex-col"}>
             <TextSpan />
-            <div className={"justify-center items-center gap-5 flex flex-col flex-1"}>
-                <h1 className={"text-3xl"}>This section is a work in progress.</h1>
-                <div className={"flex flex-row items-center gap-5"}>
-                    <p className={"text-xl"}>For now please refer to my GitHub account:</p>
-                    <SocialLink icon={<SiGithub size={40} />}
-                                link={"https://github.com/gloggers99"} />
+            <div className={"flex flex-col flex-1"}>
+                <div className={"ml-10 font-playfair"}>
+                    <h1 className={"md:text-4xl text-2xl"}>What I'm working on:</h1>
+                    <small className={"font-opensans"}>This page is actively changed to reflect my two primary projects. </small>
                 </div>
-                <div className={"flex flex-row items-center gap-5"}>
-                    <p className={"text-xl"}>For inquiries please contact me via email:</p>
-                    <SocialLink icon={<SiGmail size={40}/>}
-                                link={"mailto:lucas.marta0799@gmail.com"} />
+                <div className={"flex-1 flex md:flex-row flex-col gap-5 p-5"}>
+                    <PortfolioItem name={"Howard Wettan's Campaign Website"}
+                                   description={"I am currently managing Howard Wettan's campaign website!"}
+                                   photo={<Image alt={"Howard"} src={"/howard.png"} width={500} height={500}/>} />
+                    <PortfolioItem name={"Sonder"}
+                                   description={"My personal project Sonder seeks to fill gaps in current finance information aggregation tools. " +
+                                       "There is much left to do, including a collaboration with William Chastain."}
+                                   photo={<Image alt={"Sonder"} src={"/sonder.png"} width={500} height={500} />} />
                 </div>
             </div>
         </div>
